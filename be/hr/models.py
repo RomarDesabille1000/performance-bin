b520ba10eaf89f6a678d228d79d5536db520d1f7
romar22 <romardesabille@gmail.com>
2022-12-04 13:16:06 +0800

    editable rubric

--- be/hr/models.py
+++ be/hr/models.py
@@ -15,6 +15,7 @@ class EvaluationRubric(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     percentage = models.IntegerField()
+    editable = models.BooleanField(default=True)
 
     def __str__(self):
             return self.name
