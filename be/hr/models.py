93d616b23aeb230db050b5f8ae44ecb03c577280
romar22 <romardesabille@gmail.com>
2022-12-04 16:31:54 +0800

    backjob & sales model

--- be/hr/models.py
+++ be/hr/models.py
@@ -38,4 +38,17 @@ class EmployeeEvaluationDetail(models.Model):
     percentage = models.IntegerField()
     score = models.DecimalField(default=0, max_digits=20, decimal_places=2)
 
-    
+
+class BackJobs(models.Model):
+    user = models.ForeignKey('users.User', 
+        on_delete=models.CASCADE, blank=True, null=True, related_name='employee_backjobs')
+    customer_name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    date = models.DateTimeField(default=now)
+
+class Sales(models.Model):
+    user = models.ForeignKey('users.User', 
+        on_delete=models.CASCADE, blank=True, null=True, related_name='employee_sales')
+    date = models.DateTimeField(default=now)
+    item_deal = models.CharField(max_length=255)
+    amount = models.DateTimeField(default=now)
