d4aa4f45225d781024d185a528e80e47129004aa
romar22 <romardesabille@gmail.com>
2023-01-31 18:57:38 +0800

    update

--- fe/pages/e/index.js
+++ fe/pages/e/index.js
@@ -35,7 +35,7 @@ export default function Employee() {
     return(
         <div>
             <div className="flex justify-end px-5">
-                {status && (
+                {status && user?.user_employee?.designation === 'SUPERVISOR' && (
                     <div className="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md mt-3 max-w-[300px]">
                         <div className="flex">
                             <div>
